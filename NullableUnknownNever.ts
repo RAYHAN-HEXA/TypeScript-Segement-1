07aae564844afee819b49520d77aba039cbcb0a8
Rayhan <mohammadrayhan796@gmail.com>
2026-07-18 21:41:59 +0600

    Add never type example with error throwing function

--- NullableUnknownNever.ts
+++ NullableUnknownNever.ts
@@ -33,3 +33,12 @@ else{
 discountCalculator(100);
 discountCalculator("100 tk");
 discountCalculator(null);
+
+//void 
+
+
+const throwError= ( msg : string): never={
+    throw new  Error(msg)
+}
+
+throwError('Error.....')
