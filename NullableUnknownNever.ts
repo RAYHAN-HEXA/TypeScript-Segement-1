69006a2f05bceb5e0a103a89cdfafc44ef3f684b
Rayhan <mohammadrayhan796@gmail.com>
2026-07-18 21:42:18 +0600

    Improve code formatting and enhance comments in NullableUnknownNever.ts

--- NullableUnknownNever.ts
+++ NullableUnknownNever.ts
@@ -1,44 +1,38 @@
-//NullableUnknownNever
+// NullableUnknownNever
 
 // A nullable type is a data type that can store either a normal value or null, which means no value is available.
-const getUser = (input :string | null) ={
- if(input){
+const getUser = (input: string | null) => {
+  if (input) {
     console.log(`from db : ${input}`);
-
- }
- else{
+  } else {
     console.log("from db  : all user ")
- }
+  }
 }
 
 getUser(null)
 
-// unnkown 
+// unknown type example
 
-const discountCalculator  = (input : unknown)= {
-if(typeof input === "number"){
-    const discountPrice =  input*0.1;
+const discountCalculator = (input: unknown) => {
+  if (typeof input === "number") {
+    const discountPrice = input * 0.1;
     console.log(discountPrice)
-}else if( typeof input === "string"){
-    const [discountedprice] = input.split(" ");// splite the   all string  and only recieve index 0 vallue 
+  } else if (typeof input === "string") {
+    const [discountedprice] = input.split(" "); // split the string and receive index 0 value
     console.log(Number(discountedprice) * 0.1)
-
-
-}
-else{
+  } else {
     console.log(" Wrong Input ")
-}
+  }
 }
 
 discountCalculator(100);
 discountCalculator("100 tk");
 discountCalculator(null);
 
-//void 
+// never type example
 
-
-const throwError= ( msg : string): never={
-    throw new  Error(msg)
+const throwError = (msg: string): never => {
+  throw new Error(msg)
 }
 
 throwError('Error.....')
